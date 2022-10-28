b32cefa8cad2398ce73183b0e8405687853b1dda
mjaquiery <matt.jaquiery@cs.ox.ac.uk>
2022-10-28 12:21:49 +0100

    rollup tweak

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       manualChunks: {
         questionnaire: ["questionnaire-core"],
         cis_r: ["questionnaire-cis-r"],
-        pecunia: ["questionnaire-pecunia"],
+        // pecunia: ["questionnaire-pecunia"],
       },
     },
   },
